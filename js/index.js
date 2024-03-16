b5d979fa48c9017bf4744316de21b9b0b42327e5
koudaihirata <2230051@ecc.ac.jp>
2024-03-17 00:02:30 +0900

    修正

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-const btn = document.querySelector('button');
-console.log(btn);
+// const btn = document.querySelector('button');
+// console.log(btn);
 
-btn.addEventListener('click', () => {
-  console.log('You clicked me');
+// btn.addEventListener('click', () => {
+//   console.log('You clicked me');
 
-  window.location.href = 'cardEditing.html';
-});
+//   window.location.href = 'cardEditing.html';
+// });
